65701444e64237d0b426cc2fce7ccb033ee0a513
Charles Khervie Realino <charleskhervie@gmail.com>
2026-06-17 22:16:25 +0800

    delete

--- extract_pdf.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-pdf_path = r"D:\Homelab\site\gdocs with image\homelab.pdf"
-
-with pdfplumber.open(pdf_path) as pdf:
-    for page in pdf.pages:
-        print(page.extract_text())
